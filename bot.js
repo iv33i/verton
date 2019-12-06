b4966346f7631b4a6a0b122e9b861a1eb66c8fd9
iv33i <58321083+iv33i@users.noreply.github.com>
2019-12-06 14:32:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,21 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+client.on('message', message => {
+    if (message.content.startsWith(".avatar")) {
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
